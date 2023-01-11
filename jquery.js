9017df83f4125275b57dee3bfd5277c7c9d172a0
Devlin Jenkins <djdevj@gmail.com>
2023-01-10 18:01:29 -0800

    added pseudo to JQuery

---
+++ jquery.js
@@ -0,0 +1,6 @@
+// activate save button so that when it is clicked, it will commit the text written in the form next to it in the local storage.
+    // save = (click, move text to local storage)
+// When the text is commited to the local storgage, it will show up automatically upon refresh (stay put)
+    // stay-put = (text moved to local storage, text displayed in place upon screen refresh)
+// Make it so that the colors of the form change color based on the time of day matching the form (past, present future)
+// Colors for the form should match the theme of the calendar (3 colors)
